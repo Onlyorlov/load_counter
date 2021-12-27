cc4110bc99e89d196d22c53c9e4edc31be6024ca
orlov <onlyorlov@gmail.com>
2021-12-27 15:41:51 +0300

    reverse

--- track.py
+++ track.py
@@ -146,8 +146,7 @@ def detect(opt):
                         #     #     inplace_counter+=1
                         #     pass
                     # if mask:
-                    # annotator.text([0,0], f'Число людей в зоне:{inplace_counter}')
-                    annotator.text([0,0], 'kjadsf')
+                    annotator.text([0,0], f'Число людей в зоне:{inplace_counter}')
 
 
             # Print time (inference-only)
